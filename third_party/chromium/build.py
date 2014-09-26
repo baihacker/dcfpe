50215f42bb50296f47bb8ed863ba369422d7a888
baihacker <bailiangsky@gmail.com>
2014-09-27 01:32:18 +0800

    remove file

--- third_party/chromium/build.py
+++
@@ -1,9 +0,0 @@
-import os
-gyp_path = os.path.dirname(os.path.realpath(__file__))
-gyp_path = os.path.abspath(os.path.join(gyp_path, r'..\..\tools\gyp\pylib'))
-os.environ.setdefault('GYP_MSVS_VERSION', '2013')
-os.environ.setdefault('GYP_GENERATORS', 'msvs')
-os.environ.setdefault('GYP_DEFINES', 'component=shared_library')
-os.environ.setdefault('GYP_PATH', gyp_path)
-print gyp_path
-os.system(r'python build\gyp_chromium base\base.gyp --depth=.')
