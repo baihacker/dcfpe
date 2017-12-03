57c660e18d6f9fd11c254d1d0a9c86d3fe41034c
baihacker <bailiangsky@gmail.com>
2017-12-03 09:27:21 -0800

    Add missing files.

---
+++ src/remote_shell/listener_node.cc
@@ -0,0 +1,44 @@
+#include "remote_shell/listener_node.h"
+#include "remote_shell/proto/rs.pb.h"
+#include "remote_shell/command_executor.h"
+
+namespace rs
+{
+
+ListenerNode::ListenerNode(
+    const std::string& myIP):
+    ServerNode(myIP),
+    nextSessionId(0),
+    weakptr_factory_(this)
+  {
+
+  }
+
+ListenerNode::~ListenerNode()
+{
+}
+
+bool ListenerNode::listen() {
+  return ServerNode::start(kRSListenerPort);
+}
+
+void ListenerNode::handleRequest(const Request& req, Response& reply)
+{
+  reply.set_error_code(-1);
+  if (req.has_create_session()) {
+    auto sessionId = nextSessionId++;
+    const auto& detail = req.create_session();
+    const auto& host = detail.address();
+    //std::string cmd = "rs --host=\"" + host + "\" -l=0";
+    //printf(cmd.c_str());
+    //system(cmd.c_str());
+    auto wcmd = base::UTF8ToNative("rs --host=\"" + host + "\" -l=0 --sid=" + std::to_string(sessionId));
+    STARTUPINFO                           si_ = {0};
+    PROCESS_INFORMATION                   process_info_ = {0};
+    CreateProcess(NULL, (wchar_t*)wcmd.c_str(),
+      NULL, NULL, false, 0, NULL, NULL, &si_, &process_info_);
+    reply.set_session_id(sessionId);
+    reply.set_error_code(0);
+  }
+}
+}
---
+++ src/remote_shell/listener_node.h
@@ -0,0 +1,24 @@
+#ifndef REMOTE_SHELL_LISTENER_NODE_H_
+#define REMOTE_SHELL_LISTENER_NODE_H_
+
+#include "remote_shell/server_node.h"
+#include "remote_shell/proto/rs.pb.h"
+
+namespace rs
+{
+static const int kRSListenerPort = 3330;
+class ListenerNode : public ServerNode, public base::RefCounted<ListenerNode>
+{
+public:
+  ListenerNode(const std::string& myIP);
+  ~ListenerNode();
+
+  bool listen();
+  void handleRequest(const Request& req, Response& reply);
+
+private:
+  int64_t nextSessionId;
+  base::WeakPtrFactory<ListenerNode>                 weakptr_factory_;
+};
+}
+#endif
