30e580253c756d0e642d6dc32028018c8bf9883d
baihacker <bailiangsky@gmail.com>
2021-04-12 19:00:42 -0700

    Format RepeatedAction.

--- src/dpe_base/utility/repeated_action.cc
+++ src/dpe_base/utility/repeated_action.cc
@@ -1,110 +1,102 @@
 #include "repeated_action.h"
 
-namespace base
-{
+namespace base {
 
-RepeatedAction::RepeatedAction(RepeatedActionHost* host) :
-  host_(host), weakptr_factory_(this), is_running_(false), current_cookie_(0), repeated_time_(-1)
-{
-}
+RepeatedAction::RepeatedAction(RepeatedActionHost* host)
+    : host_(host),
+      weakptr_factory_(this),
+      is_running_(false),
+      current_cookie_(0),
+      repeated_time_(-1) {}
 
-RepeatedAction::~RepeatedAction()
-{
-}
+RepeatedAction::~RepeatedAction() {}
 
-static void doActionInternal(std::function<void()> callback)
-{
-  callback();
-}
+static void doActionInternal(std::function<void()> callback) { callback(); }
 
-bool RepeatedAction::Start(std::function<void()> callback, int delay, int period, int32 repeated_time)
-{
+bool RepeatedAction::Start(std::function<void()> callback, int delay,
+                           int period, int32 repeated_time) {
   return Start(base::Bind(&doActionInternal, callback),
-      base::TimeDelta::FromSeconds(delay),
-      base::TimeDelta::FromSeconds(period),
-      repeated_time);
+               base::TimeDelta::FromSeconds(delay),
+               base::TimeDelta::FromSeconds(period), repeated_time);
 }
 
 bool RepeatedAction::Start(const base::Closure& action,
-        base::TimeDelta time_delay, base::TimeDelta time_interval, int32_t repeated_time)
-{
-  if (time_delay.ToInternalValue() < 0) time_delay = base::TimeDelta::FromMicroseconds(0);
+                           base::TimeDelta time_delay,
+                           base::TimeDelta time_interval,
+                           int32_t repeated_time) {
+  if (time_delay.ToInternalValue() < 0) {
+    time_delay = base::TimeDelta::FromMicroseconds(0);
+  }
   is_running_ = true;
   ++current_cookie_;
   action_ = action;
   repeated_time_ = repeated_time;
   interval_ = time_interval;
-  if (time_delay.ToInternalValue() == 0)
-  {
-    base::ThreadPool::PostTask(base::ThreadPool::UI, FROM_HERE,
-      base::Bind(&RepeatedAction::DoAction, weakptr_factory_.GetWeakPtr(), current_cookie_)
-        );
-  }
-  else
-  {
-    base::ThreadPool::PostDelayedTask(base::ThreadPool::UI, FROM_HERE,
-      base::Bind(&RepeatedAction::DoAction, weakptr_factory_.GetWeakPtr(), current_cookie_),
-      time_delay
-        );
+  if (time_delay.ToInternalValue() == 0) {
+    base::ThreadPool::PostTask(
+        base::ThreadPool::UI, FROM_HERE,
+        base::Bind(&RepeatedAction::DoAction, weakptr_factory_.GetWeakPtr(),
+                   current_cookie_));
+  } else {
+    base::ThreadPool::PostDelayedTask(
+        base::ThreadPool::UI, FROM_HERE,
+        base::Bind(&RepeatedAction::DoAction, weakptr_factory_.GetWeakPtr(),
+                   current_cookie_),
+        time_delay);
   }
   return true;
 }
 
-bool RepeatedAction::Stop()
-{
+bool RepeatedAction::Stop() {
   is_running_ = false;
   ++current_cookie_;
   return true;
 }
 
-bool RepeatedAction::Restart(base::TimeDelta time_delay, base::TimeDelta time_interval, int32_t repeated_time)
-{
-  if (time_delay.ToInternalValue() < 0) time_delay = base::TimeDelta::FromMicroseconds(0);
+bool RepeatedAction::Restart(base::TimeDelta time_delay,
+                             base::TimeDelta time_interval,
+                             int32_t repeated_time) {
+  if (time_delay.ToInternalValue() < 0)
+    time_delay = base::TimeDelta::FromMicroseconds(0);
   if (!is_running_) return false;
   ++current_cookie_;
   repeated_time_ = repeated_time;
   interval_ = time_interval;
-  if (time_delay.ToInternalValue() == 0)
-  {
-    base::ThreadPool::PostTask(base::ThreadPool::UI, FROM_HERE,
-      base::Bind(&RepeatedAction::DoAction, weakptr_factory_.GetWeakPtr(), current_cookie_)
-        );
-  }
-  else
-  {
-    base::ThreadPool::PostDelayedTask(base::ThreadPool::UI, FROM_HERE,
-      base::Bind(&RepeatedAction::DoAction, weakptr_factory_.GetWeakPtr(), current_cookie_),
-      time_delay
-        );
+  if (time_delay.ToInternalValue() == 0) {
+    base::ThreadPool::PostTask(
+        base::ThreadPool::UI, FROM_HERE,
+        base::Bind(&RepeatedAction::DoAction, weakptr_factory_.GetWeakPtr(),
+                   current_cookie_));
+  } else {
+    base::ThreadPool::PostDelayedTask(
+        base::ThreadPool::UI, FROM_HERE,
+        base::Bind(&RepeatedAction::DoAction, weakptr_factory_.GetWeakPtr(),
+                   current_cookie_),
+        time_delay);
   }
   return true;
 }
 
-void RepeatedAction::DoAction(base::WeakPtr<RepeatedAction> self, int32_t cookie)
-{
+void RepeatedAction::DoAction(base::WeakPtr<RepeatedAction> self,
+                              int32_t cookie) {
   if (auto* pThis = self.get()) pThis->DoActionImpl(cookie);
 }
 
-void RepeatedAction::DoActionImpl(int32_t cookie)
-{
-  if (is_running_ && cookie == current_cookie_)
-  {
-    if (repeated_time_ == 0)
-    {
-      if (host_)
-      {
+void RepeatedAction::DoActionImpl(int32_t cookie) {
+  if (is_running_ && cookie == current_cookie_) {
+    if (repeated_time_ == 0) {
+      if (host_) {
         host_->OnRepeatedActionFinish(this);
       }
-    }
-    else if (repeated_time_ < 0 || repeated_time_--)
-    {
+    } else if (repeated_time_ < 0 || repeated_time_--) {
       action_.Run();
-      base::ThreadPool::PostDelayedTask(base::ThreadPool::UI, FROM_HERE,
-        base::Bind(&RepeatedAction::DoAction, weakptr_factory_.GetWeakPtr(), current_cookie_),
-        interval_
-        );
+      base::ThreadPool::PostDelayedTask(
+          base::ThreadPool::UI, FROM_HERE,
+          base::Bind(&RepeatedAction::DoAction, weakptr_factory_.GetWeakPtr(),
+                     current_cookie_),
+          interval_);
     }
   }
 }
 
-}
+}  // namespace base
--- src/dpe_base/utility/repeated_action.h
+++ src/dpe_base/utility/repeated_action.h
@@ -5,42 +5,42 @@
 #include "dpe_base/chromium_base.h"
 #include "dpe_base/thread_pool.h"
 
-namespace base
-{
+namespace base {
 class RepeatedAction;
-class RepeatedActionHost
-{
-public:
+class RepeatedActionHost {
+ public:
   virtual void OnRepeatedActionFinish(RepeatedAction* ra) = 0;
-  virtual ~RepeatedActionHost(){}
+  virtual ~RepeatedActionHost() {}
 };
 
-class DPE_BASE_EXPORT RepeatedAction : public base::RefCounted<RepeatedAction>
-{
-public:
+class DPE_BASE_EXPORT RepeatedAction : public base::RefCounted<RepeatedAction> {
+ public:
   RepeatedAction(RepeatedActionHost* host);
   ~RepeatedAction();
 
-  bool Start(std::function<void()> callback, int delay, int period, int repeated_time);
-  bool Start(const base::Closure& action, base::TimeDelta time_delay, base::TimeDelta time_interval, int32_t repeated_time);
+  bool Start(std::function<void()> callback, int delay, int period,
+             int repeated_time);
+  bool Start(const base::Closure& action, base::TimeDelta time_delay,
+             base::TimeDelta time_interval, int32_t repeated_time);
   bool Stop();
-  bool Restart(base::TimeDelta time_delay, base::TimeDelta time_interval, int32_t repeated_time);
-  
+  bool Restart(base::TimeDelta time_delay, base::TimeDelta time_interval,
+               int32_t repeated_time);
+
   static void DoAction(base::WeakPtr<RepeatedAction> self, int32_t cookie);
   void DoActionImpl(int32_t cookie);
 
-  bool IsRunning() const {return is_running_;}
-  base::TimeDelta Interval() const {return interval_;}
+  bool IsRunning() const { return is_running_; }
+  base::TimeDelta Interval() const { return interval_; }
 
-private:
-  RepeatedActionHost*                                   host_;
-  bool                                                  is_running_;
-  int32_t                                               current_cookie_;
-  base::Closure                                         action_;
-  base::TimeDelta                                       interval_;
-  int32_t                                               repeated_time_;
-  base::WeakPtrFactory<RepeatedAction>                  weakptr_factory_;
+ private:
+  RepeatedActionHost* host_;
+  bool is_running_;
+  int32_t current_cookie_;
+  base::Closure action_;
+  base::TimeDelta interval_;
+  int32_t repeated_time_;
+  base::WeakPtrFactory<RepeatedAction> weakptr_factory_;
 };
-}
+}  // namespace base
 
 #endif
